f9160ca7e85b83e854ebd5c2736941a1d6fece76
neha22oct97 <neha22oct97@gmail.com>
2021-03-28 22:52:24 +0530

    Javascript

---
+++ Javascript/Default.js
@@ -0,0 +1,5 @@
+
+document.write("<h1>External file</h1>");
+console.log("Hey Prem");
+var a=10;
+
---
+++ Javascript/Tut1.js
@@ -0,0 +1,10 @@
+
+//1. Ending statements with semicolon is not required, but highly recommended.
+
+var a, b, c;     // Declare 3 variables
+a = 5;           // Assign the value 5 to a
+b = 6;           // Assign the value 6 to b
+c = a + b;       // Assign the sum of a and b to c
+
+//2. When separated by semicolons, multiple statements on one line are allowed:
+a = 5; b = 6; document.write("<h1>Hello World</h1>") ;
